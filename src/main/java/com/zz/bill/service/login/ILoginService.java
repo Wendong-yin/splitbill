117493a26200ce32e4f28f279d8948f1b5b2ea0b
wendong <hi.yinwendong@gmail.com>
2018-06-06 18:28:05 -0400

    补充注册成功的 code 和 msg

--- src/main/java/com/zz/bill/service/login/ILoginService.java
+++ src/main/java/com/zz/bill/service/login/ILoginService.java
@@ -1,5 +1,6 @@
 package com.zz.bill.service.login;
 
+import com.zz.bill.CommonCode;
 import com.zz.bill.model.JsonResult;
 import com.zz.bill.model.account.UserInfo;
 import com.zz.bill.service.user.IUserService;
@@ -9,14 +10,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ILoginService implements ILogin {
 
+    // ❤️ 这个为什么有黄线
     @Autowired
     private IUserService accountService;
-
 
     @Override
     public JsonResult register(UserInfo userInfo) throws Exception {
         UserInfo user = accountService.addUser(userInfo);
         return JsonResult.builder()
+                .code(CommonCode.SUCC.getCode())
+                .msg(CommonCode.SUCC.getMessage())
                 .data(user)
                 .build();
     }
